6facf24a54ac645b40e4a479ac553cdc6c3eb033
Borys2 <53047945+Borys2@users.noreply.github.com>
2023-01-06 22:39:12 -0500

    Delete hello.py

--- hello.py
+++
@@ -1,9 +0,0 @@
-
-firstName = "Borys"
-secondName = "Dekhtyarenko"
-<<<<<<< HEAD
-massage =  firstName + " " + secondName + " " + "wos written his first coomit congratulations".strip()
-=======
-massage =  firstName + " " + secondName + " " + "wos written his first coomit congratulations!"
->>>>>>> 6f88860 (add exclamation mark)
-print(massage)
