23aa90a4c6f75072a70052ff1cb1b88b60e9c8a0
Borys2 <53047945+Borys2@users.noreply.github.com>
2023-01-06 22:00:00 -0500

    add function which remove space

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 
 firstName = "Borys"
 secondName = "Dekhtyarenko"
-massage =  firstName + " " + secondName + " " + "wos written his first coomit congratulations"
+massage =  firstName + " " + secondName + " " + "wos written his first coomit congratulations".strip()
 print(massage)
