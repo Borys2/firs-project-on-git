d975d16422e3b1d5ccf1b0ceb87c7698c4259838
Borys Dekhtyarenko <borys@Boryss-MacBook-Air.local>
2023-01-06 22:17:13 -0500

    Fix conflict

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 
 firstName = "Borys"
 secondName = "Dekhtyarenko"
+<<<<<<< HEAD
 massage =  firstName + " " + secondName + " " + "wos written his first coomit congratulations".strip()
+=======
+massage =  firstName + " " + secondName + " " + "wos written his first coomit congratulations!"
+>>>>>>> 6f88860 (add exclamation mark)
 print(massage)
